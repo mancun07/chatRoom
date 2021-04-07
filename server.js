1a9d101f989208d3e24e1f591c61ee566c0f8cb7
Dima <address>
2021-04-07 13:39:41 +0300

    some corrections

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on("connection", (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit("message", formatMessage(Admin, "Welcome to ChatCord!"));
+    socket.emit("message", formatMessage(Admin, "Welcome to SportChat!"));
 
     // Broadcast when a user connects
     socket.broadcast
